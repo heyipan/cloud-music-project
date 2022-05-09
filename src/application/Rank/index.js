c2b56324bc1fcf1f64abf6f8e4b82b4dd3fada41
25042 <qiheng.he@hand-china.com>
2022-05-09 23:26:41 +0800

    feat: 调整排行榜展示逻辑

--- src/application/Rank/index.js
+++ src/application/Rank/index.js
@@ -1,7 +1,13 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
+import { Outlet, useRoutes } from "react-router-dom";
 
 import { getRankList } from "./store";
+import Scroll from "../../components/scroll";
+import { filterIndex } from '../../api/utils';
+import Loading from "../../baseUI/loading";
+
+import { List, ListItem, SongList, Container } from './style'
 
 function Rank(props) {
   const { rankList: list, loading } = props;
@@ -12,7 +18,64 @@ function Rank(props) {
     getRankListDataDispatch();
   }, []);
 
-  let rankList = list ? list.toJS() : [];
+  const rankList = list ? list.toJS() : [];
+
+  const globalStartIndex = filterIndex (rankList);
+  const officialList = rankList.slice (0, globalStartIndex);
+  const globalList = rankList.slice (globalStartIndex);
+
+  // 这是渲染榜单列表函数，传入 global 变量来区分不同的布局方式
+  const renderRankList = (list, global) => {
+    return (
+      <List globalRank={global}>
+        {
+        list.map((item) => {
+          return (
+            <ListItem key={item.id} tracks={item.tracks}>
+              <div className="img_wrapper">
+                <img src={item.coverImgUrl} alt=""/>
+                <div className="decorate"></div>
+                <span className="update_frequecy">{item.updateFrequency}</span>
+              </div>
+              { renderSongList(item.tracks)  }
+            </ListItem>
+          )
+        })
+      } 
+      </List>
+    )
+  }
+  
+  const renderSongList = (list) => {
+    return list.length ? (
+      <SongList>
+        {
+          list.map((item, index) => {
+            return <li key={index}>{index+1}. {item.first} - {item.second}</li>
+          })
+        }
+      </SongList>
+    ) : null;
+  }
+  
+  // 榜单数据未加载出来之前都给隐藏
+  let displayStyle = loading ? {"display":"none"}:  {"display": ""};
+  
+  return (
+    <Container>
+      <Scroll>
+        <div>
+          <h1 className="offical" style={displayStyle}> 官方榜 </h1>
+            { renderRankList (officialList) }
+          <h1 className="global" style={displayStyle}> 全球榜 </h1>
+            { renderRankList (globalList, true) }
+            <Loading show={loading} />
+        </div>
+      </Scroll> 
+      {/* {renderRoutes(props.route.routes)} */}
+      <Outlet />
+    </Container>
+    );
 }
 
 // 映射 Redux 全局的 state 到组件的 props 上
